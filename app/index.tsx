71b51310081a8f951aeba60d86cda4518e54569b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-12-22 10:40:26 +0530

    Add initial project setup with Tailwind CSS and remove unused components

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import '../global.css'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-5xl text-blue-700'>index</Text>
+    </View>
+  )
+}
+
+export default index
